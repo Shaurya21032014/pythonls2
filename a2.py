ae60f518a70150aa1e31781b9b707ef497a70013
Shaurya21032014 <haritika.arora@gmail.com>
2025-11-02 15:21:56 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,25 @@
+# string type 
+text = "hey we are learning python "
+print(type(text))
+
+
+# integer type 
+marks = 80
+print(type(marks))
+
+# float type 
+cgpa = 3.345679754
+print(type(cgpa))
+
+# boolean type
+student = False 
+print(type(student))
+
+convertInt = int(cgpa)
+print(convertInt)
+
+convertFloat = float(marks )
+print(convertFloat)
+
+convertStr = str(marks)
+print(type(convertStr))
